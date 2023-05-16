c9587c3e44e6781a4c096f62366e06b9c73d659a
caseyfee <caseyfee3@gmail.com>
2023-05-15 15:05:08 -1000

    all working, no errors!

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,13 +18,13 @@ export const postDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ jate: content });
+  const request = store.add({ id: 1, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 // Added logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('Update to the database');
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -33,7 +33,7 @@ export const putDb = async (id, content) => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, jate: content });
   // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -51,11 +51,11 @@ export const getDb = async () => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 
 }
 
